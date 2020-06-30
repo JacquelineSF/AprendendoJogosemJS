0deca1153c9de10210c9110754a385ec8e78917f
Jacqueline dos Santos <jacquelinevps@hotmail.com>
2020-06-30 10:03:35 -0300

    #

--- AnimaçaoPersonagm/script.js
+++ AnimaçaoPersonagm/script.js
@@ -2,8 +2,34 @@ window.onload = function(){
     let left = 65, up = 87, rigth = 68, down = 83
     let canv = document.querySelector("canvas")
     let contxt = canv.getContext("2d")
-    
-    let person = new Sprite()
+    let spriteSheet = new Image()
+    spriteSheet.src = "imagem/imgsprite.png"
+    let person = new Sprite(spriteSheet)
 
+
+
+spriteSheet.onload = function(){
+    Inicio()
+}
+
+function Inicio(){
+    loop()
+}
+
+    function update(){
+
+
+    }
+    function draw(){
+        person.draw(context)
+
+    }
+
+
+    function loop(){
+        window.requestAnimationFrame(loop, canv)
+        update()
+        draw()
+    }
 
 }
--- AnimaçaoPersonagm/scriptsprite.js
+++ AnimaçaoPersonagm/scriptsprite.js
@@ -1,5 +1,16 @@
-function Sprite(){
+function Sprite(img){
     //atributos 
-    this.srcX = this.srcY
-    //metodos
+    this.srcX = this.srcY = 0
+    //tamanho do personagem, na imagem
+    this.width = 400
+    this.height = 400
+    //para mover o personagem na tela
+    this.posX = this.posY = 0
+    this.img = img
+    //metodos pra desenhar o personagem na tela
+
+    this.draw = function(context){
+        context.drawImage(this.img, this.srcX, this.srcY,this.width, this.height, this.posX, this.posY, this.width, this.height)
+    }
+    
 }
