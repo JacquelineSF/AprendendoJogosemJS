677feb4eb1b3219e2de3ea7f90c0e205eebccd09
Jacqueline dos Santos <jacquelinevps@hotmail.com>
2020-06-30 11:51:45 -0300

    #

--- AnimaçaoPersonagm/script.js
+++ AnimaçaoPersonagm/script.js
@@ -3,8 +3,58 @@ window.onload = function(){
     let canv = document.querySelector("canvas")
     let contxt = canv.getContext("2d")
     let spriteSheet = new Image()
-    spriteSheet.src = "imagem/img.png"
+    spriteSheet.src = "imagem/imgsprite.png"
     let person = new Sprite(spriteSheet)
+    let posX = 0
+    let posY = 0
+    let scene = new Image()
+    scene.src = "imagem/scene.png"
+
+
+    window.addEventListener('keydown',apertabotao,false)
+    window.addEventListener('keyup',soltabotao,false)
+    
+
+//para n ter conflito entre as teclas, vc coloca a priemira como true e as outras como falso.
+    function apertabotao(e){
+        switch (e.keyCode){
+            case left: person.movLeft = true
+                        person.movUp = false
+                        person.movRight = false
+                        person.movDown = false
+            break
+
+            case up: person.movUp = true
+                        person.movLeft = false
+                        person.movRight = false
+                        person.movDown = false
+            break
+            case rigth: person.movRight = true
+                        person.movUp = false
+                        person.movLeft = false
+                        person.movDown = false
+            break
+            case down: person.movDown = true
+                        person.movUp = false
+                        person.movRight = false
+                        person.movLeft = false
+        }
+    }
+    function soltabotao(e){
+        switch (e.keyCode){
+            case left: person.movLeft = false
+                    
+            break
+
+            case up: person.movUp = false
+                   
+            case rigth: person.movRight = false
+                        
+            break
+            case down: person.movDown = false
+                    
+        }
+    }
 
 
 
@@ -13,14 +63,17 @@ spriteSheet.onload = function(){
 }
 
 function Inicio(){
+    //se quiser mudar a posicao de inicio: person.posX = person.posY = 150
     loop()
 }
 
     function update(){
-
+        person.move()
 
     }
     function draw(){
+        contxt.clearRect(0,0, canv.width,canv.height)
+        contxt.drawImage(scene, 0,0,scene.width, scene.height,0,0 ,canv.width, canv.height)
         person.draw(contxt)
 
     }
--- AnimaçaoPersonagm/scriptsprite.js
+++ AnimaçaoPersonagm/scriptsprite.js
@@ -1,16 +1,60 @@
 function Sprite (img){
     //atributos 
+    this.movLeft = this.movUp = this.movRight = this.movDown = false
     this.srcX = this.srcY = 0
     //tamanho do personagem, na imagem
-    this.width = 24
-    this.height = 32
+    this.width = 49
+    this.height = 73
     //para mover o personagem na tela
     this.posX = this.posY = 0
     this.img = img
+    this.speed = 1
+    this.contadorAnima = 0   
     //metodos pra desenhar o personagem na tela
 
     this.draw = function(contxt){
-        context.drawImage(this.img, this.srcX, this.srcY,this.width, this.height, this.posX, this.posY, this.width, this.height)
+        contxt.drawImage(this.img, this.srcX, this.srcY,this.width, this.height, this.posX, this.posY, this.width, this.height)
+        this.Animation()
     }
     
-}
+
+
+    this.move = function(){
+        
+        if(this.movRight){
+           
+                this.posX+= this.speed
+                this.srcY = this.height * 3
+                
+            }else 
+            if(this.movLeft){
+        
+            this.posX -= this.speed
+            this.srcY = this.height * 2
+        }else
+            if(this.movUp){
+  
+                this.posY-= this.speed
+                this.srcY = this.height * 1
+            
+         }else
+        if(this.movDown){
+        
+            this.posY+= this.speed
+            this.srcY = this.height * 0
+        
+    }
+    }
+
+    this.Animation = function(){
+        if(this.movLeft || this.movUp || this.movRight || this.movDown){
+            this.contadorAnima++
+            if(this.contadorAnima >= 17) {
+                this.contadorAnima = 0
+            }
+            this.srcX = Math.floor(this.contadorAnima / 5) * this.width
+        }
+    }
+    }
+
+//esse /5 é a velocidade de animacao, para aumentar a velocidade é só diminuir o valor
